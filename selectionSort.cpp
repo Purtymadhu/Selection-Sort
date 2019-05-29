f331376bd5c36306cc11a61ba43fe04eb26dd1f1
Madhu purty <42269231+Purtymadhu@users.noreply.github.com>
2019-05-29 15:22:39 +0530

    Add files via upload

---
+++ selectionSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std; 
+  
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+   
+    for (i = 0; i < n-1; i++)  
+    {   
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i=0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+    
+int main()  
+{  
+	int m;
+	cout<<"Enter number of elements you want in an array:\n";
+	cin>>m;
+    int arr[m];
+	cout<<"Enter elements of an array:\n";
+	for(int i=0;i<m;i++)
+	{
+		cin>>arr[i];
+	}  
+    selectionSort(arr, m);  
+    cout << "Sorted array: \n";  
+    printArray(arr, m);  
+    return 0;  
+}  
